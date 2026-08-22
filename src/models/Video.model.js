672cd0ae586c58179b1b0ae0969270bcf39e6719
Numan <iamnumanriaz@gmail.com>
2026-08-22 23:29:19 +0500

    user model is creaded for video streaming website

---
+++ src/models/Video.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+    videoFile:{
+        type: String,
+        required: true
+    },
+    thumbnail:{
+        type: String,
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    discription:{
+        type: String,
+        required: true
+    },
+    duration:{
+        type: Number, //form claudnary
+        required: true
+    },
+    duration:{
+        type: Number, 
+        required: true,
+        default: 0
+    },
+    isPublished:{
+        type: boolean, 
+        default: true
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+
+}, { timestamps: true })
+
+export const Video = mongoose.model("Video", VideoSchema)
