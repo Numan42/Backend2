f0f094d899ea6a64496b77536a63fece8eef881c
Numan <iamnumanriaz@gmail.com>
2026-09-01 23:19:07 +0500

    middleware injected

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import { UserRegister } from "../controllers/user.controller.js";
+import {upload} from '../middlewares/multer.js'
+
 const router = Router();
 
-router.route("/register").post(UserRegister);
+router.route("/register").post(  // allows to send multipart/form-data request with multiple files
+    upload.fields([
+        { name: "avatar", maxCount: 1 },
+        { name: "cover", maxCount: 1 }
+    ]),
+    UserRegister);
 
 export default router;
