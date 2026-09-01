561e87c8a02903ee0089e5fb215642c94ee94fee
Numan <iamnumanriaz@gmail.com>
2026-09-01 23:19:56 +0500

    user registion

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,14 @@
 import { asyncHandeler } from '../utils/asyncHandeler.js';
-
+import ApiError from '../utils/ApiError.js';
 export const UserRegister = asyncHandeler(async (req, res) => {
-    res.status(200).json({
-        message: 'User registered successfully',
-    })
+
+    const { username, email, fullName, password } = req.body;
+    // if (username || email || fullName || password) {
+    //     throw new ApiError(400, "All fields are required")
+    // }
+
+    // or use this 
+    if([username, email, fullName, password].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required")
+    }
 })
